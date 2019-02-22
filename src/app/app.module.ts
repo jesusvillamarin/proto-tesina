137f15bed136c26a8354a7c4d1ab768f22b784d5
Jesús Villamarín <jvillamarin@mediomelon.mx>
2019-02-22 12:19:59 -0600

    (firebase): integración de firebase para el hosting de la app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,24 @@
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import * as Hammer from 'hammerjs';
+
+
+import {
+  HammerGestureConfig,
+  HAMMER_GESTURE_CONFIG
+} from '@angular/platform-browser';
+export class MyHammerConfig extends HammerGestureConfig {
+  overrides = <any>{
+    // override hammerjs default configuration
+    swipe: { direction: Hammer.DIRECTION_ALL }
+  };
+}
 
 const COMMON_MODULES = [
   BrowserModule,
@@ -15,11 +28,9 @@ const COMMON_MODULES = [
 ];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
+  declarations: [AppComponent],
   imports: COMMON_MODULES,
-  providers: [],
+  providers: [{ provide: HAMMER_GESTURE_CONFIG, useClass: MyHammerConfig }],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/menu-nav/menu-nav.module.ts
+++ src/app/components/menu-nav/menu-nav.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MatButtonModule } from '@angular/material/button';
-import { MenuNavComponent } from './menu-nav.component'
+import { MenuNavComponent } from './menu-nav.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 
 const COMMON_MODULES = [
   CommonModule,
-  MatButtonModule
+  MatButtonModule,
+  MatToolbarModule
 
 ]
 
